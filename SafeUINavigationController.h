1e07c38709fc9bd402f86250aaf3ca47b5092835
Casey Persson <caseyp@procore.com>
2014-10-24 13:10:17 -0700

    Disable NSLog debug messages by default.

--- SafeUINavigationController.h
+++ SafeUINavigationController.h
@@ -6,7 +6,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define SAFE_NAV_DEBUG
+//#define SAFE_NAV_DEBUG
 
 #ifdef SAFE_NAV_DEBUG
 #define DebugLog(fmt,...) NSLog(@"%@",[NSString stringWithFormat:(fmt), ##__VA_ARGS__]);
